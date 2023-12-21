2fb2a08c61c4dd2b42ff8d45227d04aa076cf1e1
robertochello <robertochello@gmail.com>
2023-12-21 16:07:51 +0100

    a

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // Aggiungi l'evento di click all'immagine
 document.getElementById('clickableImage').addEventListener('click', function() {
     // Reindirizza alla pagina home.html
-    window.location.href = 'home.html';
+    window.location.href = 'page2/home.html';
 });
 
+window.onload = function () {
+    // Aggiungi un ritardo (in millisecondi) se necessario prima di mostrare la seconda immagine
+    setTimeout(function () {
+        // Creazione di un nuovo elemento immagine
+        var secondImage = document.createElement("img");
+        // Impostazione dell'ID e del percorso dell'immagine
+        secondImage.id = "secondImage";
+        secondImage.src = "images/cliccami.png";
+        secondImage.alt = "Seconda Immagine";
+
+        secondImage.classList.add("styledImage");
+
+        // Aggiunta dell'immagine al corpo del documento
+        document.body.appendChild(secondImage);
+    }, 2000); // Esempio di ritardo di 2 secondi (2000 millisecondi)
+};
